afa3bc71be15b686f4562ec80c717b045321e4ca
Samarth-77 <samarth@veersatech.com>
2024-04-23 10:45:03 +0530

    Revert "Modal,header,tableheader added in ui-lib and pages added in vperform-react"
    This reverts commit d61aa4a487f77bd0b01a6e41f46b676236c8e873.

--- apps/ui-lib/src/lib/index.ts
+++
@@ -1,9 +0,0 @@
-import PageHeader from "./PageHeader";
-import TableHeader from "./Table";
-import Modal from "./Modal";
-
-export {
-    PageHeader,
-    TableHeader,
-    Modal
-}
--- apps/vperform-react/tailwind.config.js
+++ apps/vperform-react/tailwind.config.js
@@ -11,11 +11,7 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {colors:{
-      'theme-blue':'#4364D0',
-      'custom-table-row':'#F9F9F9',
-      'custom-table-header':'#F0F7FF'
-    }},
+    extend: {},
   },
   plugins: [],
 };
