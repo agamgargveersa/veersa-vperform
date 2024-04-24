afad9b8e6de51ee020dff8ad83031869dd23e0b1
vaniveersa <vani@veersatech.com>
2024-04-24 11:29:24 +0530

    EmployeeList

--- apps/vperform-react/src/app/app.tsx
+++ apps/vperform-react/src/app/app.tsx
@@ -1,12 +1,14 @@
 import NxWelcome from './nx-welcome';
 import {ConfirmationPopUp,PageTemplate} from '../../../ui-lib/src/lib/index'
+// import AppraiseeView from './AppraiseeView';
+import EmployeeList from './EmployeeList';
+import AppraiseeView from './AppraiseeView';
 
  
 export function App() {
   return (
     <div>
-      {/* <NxWelcome title="vperform-react" /> */}
-     <PageTemplate/>
+      
       
     </div>
   );
