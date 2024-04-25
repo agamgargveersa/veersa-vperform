be9766b7b769f8a7a5215573df11e36f7e095869
dhruvjVeersa <dhruv.jain@veersatech.com>
2024-04-25 14:33:14 +0530

    configuration form

--- apps/vperform-react/src/app/app.tsx
+++ apps/vperform-react/src/app/app.tsx
@@ -1,11 +1,12 @@
 import NxWelcome from './nx-welcome';
 import InputField from '../../../ui-lib/src/lib/InputField'
+import DateRangePicker from '../../../ui-lib/src/lib/DateRangePicker'
+import ConfigurationForm from './ConfigurationForm';
 
 export function App() {
   return (
     <div>
-      {/* <NxWelcome title="vperform-react" /> */}
-      <InputField width='full'/>
+      <NxWelcome title="vperform-react" />
     </div>
   );
 }
